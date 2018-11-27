c8db5b6949f026ec90e9bc970f13c10806a1b377
rajesh chandra <onlyrchandra@gmail.com>
2018-11-27 18:33:29 +0530

    Initial sharing of project

---
+++ Student1.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Student1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Student1
+{
+    // instance variables - replace the example below with your own
+    	 int id;//data member (also instance variable)  
+	 String name;//data member(also instance variable)  
+
+
+    /**
+     * Constructor for objects of class Student1
+     */
+    public Student1()
+    {
+        // initialise instance variables
+        id = 0;
+        String name="null";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    Student1 s1=new Student1();//creating an object of Student  
+  System.out.println(s1.id);  
+  System.out.println(s1.name);  
+
+}
